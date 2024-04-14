dcf164418e9057b8446eda414f570ba509640fa1
Joshua Peek <josh@joshpeek.com>
2024-04-13 19:28:46 -0700

    Add simple fetch test

--- test_overcast.py
+++ test_overcast.py
@@ -1,2 +1,17 @@
-def test_ok():
-    assert True
+import os
+
+import pytest
+
+from overcast import fetch_podcasts
+
+
+@pytest.fixture
+def overcast_cookie() -> str:
+    if "OVERCAST_COOKIE" not in os.environ:
+        pytest.skip("OVERCAST_COOKIE not set")
+    return os.environ["OVERCAST_COOKIE"]
+
+
+def test_fetch_podcasts(tmp_path, overcast_cookie):
+    podcasts = fetch_podcasts(cache_dir=tmp_path, cookie=overcast_cookie)
+    assert len(podcasts) > 0
